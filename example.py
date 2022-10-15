6f3363f8a2e30d6523108fea106e8ca6145ca791
Gleb Tutik <tutikgv@gmail.com>
2022-10-15 21:29:55 +0300

    Добавлено сохранение изображений

--- example.py
+++ example.py
@@ -1,3 +1,5 @@
+from utils import save_images
+from utils import make_directory
 from yandex_images_parser import Parser
 
 parser = Parser()
@@ -22,4 +24,10 @@ print("Похожие коты:")
 for cat in similar_cats:
     print(cat)
 
+# Сохраняем изображения:
+output_dir = "./images/cat"
 
+make_directory(dir_path=output_dir)  # Создаём директорию
+
+save_images(square_cat, dir_path=output_dir, prefix="original_")  # Добавляем префикс к названиям
+save_images(similar_cats, dir_path=output_dir, prefix="similar_", number_images=True)  # Добавляем префикс и нумерацию
