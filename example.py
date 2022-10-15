3c7cd8056c0e0b904557dc0d356cace653d911c9
Gleb Tutik <tutikgv@gmail.com>
2022-10-16 00:59:04 +0300

    Удалил пару строк для простоты

--- example.py
+++ example.py
@@ -1,5 +1,4 @@
 from utils import save_images
-from utils import make_directory
 from yandex_images_parser import Parser
 
 parser = Parser()
@@ -26,8 +25,6 @@ for cat in similar_cats:
 
 # Сохраняем изображения:
 output_dir = "./images/cat"
-
-make_directory(dir_path=output_dir)  # Создаём директорию
 
 save_images(square_cat, dir_path=output_dir, prefix="original_")  # Добавляем префикс к названиям
 save_images(similar_cats, dir_path=output_dir, prefix="similar_", number_images=True)  # Добавляем префикс и нумерацию
