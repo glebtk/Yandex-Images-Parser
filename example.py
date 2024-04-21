d6cc3b31e25cc2213d9aac1e2b883cd4649ce44d
Gleb Tutik <117084056+glebtk@users.noreply.github.com>
2024-04-21 21:15:32 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
-from utils import save_images
+from utils import save_images, make_directory
 from yandex_images_parser import Parser
 
-parser = Parser()
+parser = Parser(headless=False)
 
 # Find a square, medium-sized image of a cat:
 square_cat = parser.query_search(query="cat",
@@ -25,6 +25,7 @@ for cat in similar_cats:
 
 # Save the images:
 output_dir = "./images/cat"
+make_directory(output_dir)
 
 save_images(square_cat, dir_path=output_dir, prefix="original_")  # Add prefix to filenames
 save_images(similar_cats, dir_path=output_dir, prefix="similar_", number_images=True)  # Add prefix and numbering
